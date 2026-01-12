ccefabb3620467e1ab03276855e60962c7f3f159
rajendra bist <rajendrabist396@gmail.com>
2026-01-12 20:07:25 +0530

    frist commit

---
+++ DOM-Preactic/script.js
@@ -0,0 +1,56 @@
+const btn = document.getElementById("btn");
+const box = document.getElementById("animation-box");
+
+btn.addEventListener("click", myMove);
+
+function myMove() {
+  let pos = 0;
+  const id = setInterval(frame, 5);
+
+  function frame() {
+    if (pos >= 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      box.style.top = pos + "px";
+      box.style.left = pos + "px";
+    }
+  }
+}
+
+
+const btnn = document.getElementById("btnn");
+const text = document.getElementById("text");
+
+btn.addEventListener("click", () => {
+
+
+  text.style.display = "none";
+
+  setTimeout(() => {
+    text.style.display = "block";
+    text.innerText = "Launched 🚀";
+  }, 3000);
+});
+
+const form = document.getElementById("myForm");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // form submit roknay
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  if (name === "") {
+    error.innerText = "Name is required";
+    return;
+  }
+
+  if (email === "") {
+    error.innerText = "Email is required";
+    return;
+  }
+
+  error.innerText = "Form submitted successfully ✅";
+});
